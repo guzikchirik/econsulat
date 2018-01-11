95df6320eab1a1f927f850023d3c1a972658f9d4
guzikchirik <aciee@mail.ru>
2018-01-11 15:49:52 +0300

    form test

--- src/main/java/test/FormTest.java
+++ src/main/java/test/FormTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 public class FormTest extends AbstractTest{
 
   @BeforeClass(alwaysRun = true)
-  public final void login() {
-    this.v3TestPage = performLogin();
+  public void beforeClass() {
+
   }
 }
