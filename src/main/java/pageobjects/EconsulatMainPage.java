f04431c22d7ed6bcbd7f261f9b21caf5ba6d3286
guzikchirik <aciee@mail.ru>
2018-01-11 15:53:59 +0300

    econsulat mainPage

--- src/main/java/pageobjects/EconsulatMainPage.java
+++ src/main/java/pageobjects/EconsulatMainPage.java
@@ -4,11 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.Link;
-import com.infongen.selenium.annotations.PageIdentifier;
-import com.infongen.selenium.annotations.PageObject;
 
-@PageObject
-@PageIdentifier(@FindBy(id = "dp_body"))
 public class EconsulatMainPage extends BasePage {
 
   private static final By SITE_LOGO_LOCATOR = By.xpath("//*[@class = 'site-logo']");
