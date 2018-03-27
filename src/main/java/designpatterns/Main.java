c6db02b5038482a28e4e18627a27274b43396ba7
guzikchirik <aciee@mail.ru>
2018-03-27 17:35:29 +0300

    create factory design pattern, abstract design pattern, factory method design pattern

--- src/main/java/designpatterns/Main.java
+++ src/main/java/designpatterns/Main.java
@@ -11,8 +11,10 @@ import designpatterns.decorator.AlloyWheels;
 import designpatterns.decorator.BaseConfigurationCar;
 import designpatterns.decorator.Car;
 import designpatterns.decorator.Leather;
+import designpatterns.factory.CarFactory;
 import designpatterns.factorymethod.CreateTransportShip;
 import designpatterns.factorymethod.CreateTransportTrain;
+import designpatterns.factorymethod.Transport;
 import designpatterns.factorymethod.TransportCreator;
 import java.util.NoSuchElementException;
 
@@ -22,22 +24,22 @@ import java.util.NoSuchElementException;
 public class Main {
 
   public static void main(String[] args) {
-//    //builder pattern
-//    User user = new User.UserBuilder("Anton", "Chyrko").age(26).address("Denisovskaya streat")
-//        .phone("+375297721991").build();
-//    System.out.println(user.toString());
+    //builder pattern
+    User user = new User.UserBuilder("Anton", "Chyrko").age(26).address("Denisovskaya streat")
+        .phone("+375297721991").build();
+    System.out.println(user.toString());
 
-//    //decorator pattern
-//    Car car = new BaseConfigurationCar();
-//    System.out.println("Description = " + car.getDescription() + " price = " + car.getPrice());
-//    car = new AlloyWheels(new Leather(car));
-//    System.out.println("Description = " + car.getDescription() + " price = " + car.getPrice());
+    //decorator pattern
+    Car car = new BaseConfigurationCar();
+    System.out.println("Description = " + car.getDescription() + " price = " + car.getPrice());
+    car = new AlloyWheels(new Leather(car));
+    System.out.println("Description = " + car.getDescription() + " price = " + car.getPrice());
 
-//    //factory method pattern
-//    TransportCreator creator = setTransportByName("train");
-//    Transport transport = creator.createTransport();
-//    transport.deliver();
-//
+    //factory method pattern
+    TransportCreator creator = setTransportByName("train");
+    Transport transport = creator.createTransport();
+    transport.deliver();
+
 //    //for factory pattern
 //    CarFactory factory = new CarFactory();
 //    Car audi = factory.createCar("audi");
@@ -45,16 +47,16 @@ public class Main {
 //    audi.drive();
 //    toyota.drive();
 
-//    //abstract factory pattern
-//    AbstractSquadronFactory abstractSquadronFactory = setAbstractSquadronFactoryByName("human");
-//
-//    Archer archer = abstractSquadronFactory.createArcher();
-//    Mage mage = abstractSquadronFactory.createMage();
-//    Warrior warrior = abstractSquadronFactory.createWarrior();
-//
-//    archer.shoot();
-//    mage.cast();
-//    warrior.attack();
+    //abstract factory pattern
+    AbstractSquadronFactory abstractSquadronFactory = setAbstractSquadronFactoryByName("human");
+
+    Archer archer = abstractSquadronFactory.createArcher();
+    Mage mage = abstractSquadronFactory.createMage();
+    Warrior warrior = abstractSquadronFactory.createWarrior();
+
+    archer.shoot();
+    mage.cast();
+    warrior.attack();
   }
 
   private static TransportCreator setTransportByName(String transportName) {
